c479b2f57abac7a6eb72700413862050c9a1081a
Jozka Tjeris <jozkant@gmail.com>
2025-06-28 14:44:09 +0700

    new: added legends for standardized level loading

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,3 @@
-// const http = require('http');
-// const {readFile, readFileSync} = require("fs");
-
-// http.createServer(function(req, res) {
-//     if(req.url === "/new"){
-//         readFile('./index.html', function(err, data) {
-//             res.writeHead(200, {'Content-Type': 'text/html'});
-//             res.write(data);
-//             res.end();
-//         });
-//         console.log("E")
-//     }
-//     else if(req.url === "/" || req.url === "/index.html"){
-//         readFile('./index.html', function(err, data) {
-//             res.writeHead(200, {'Content-Type': 'text/html'});
-//             res.write(data);
-//             res.end();
-//         });
-//     } else if(req.url === "/styles.css"){
-//         readFile('./styles.css', function(err, data) {
-//             res.writeHead(200, {'Content-Type': 'text/css'});
-//             res.write(data);
-//             res.end();
-//         });
-//     } else if(req.url === "/script.js"){
-//         readFile('./script.js', function(err, data) {
-//             res.writeHead(200, {'Content-Type': 'application/javascript'});
-//             res.write(data);
-//             res.end();
-//         });
-//     } else {
-//         res.writeHead(404, { 'Content-Type': 'text/plain' });
-//         res.end('404 Not Found');
-//     }
-// }).listen(8080);
-
-// console.log('Server running at http://localhost:8080/');
-
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
@@ -47,6 +9,18 @@ const PORT = 3000;
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
+
+app.get('/api/legends.json', (req, res) => {
+    const levelPath = path.resolve(`./backend/legends.json`);
+
+    if(fs.existsSync(levelPath)){
+        const data = fs.readFileSync(levelPath, 'utf-8');
+        res.json(JSON.parse(data));
+    }
+    else{
+        res.status(404).json({error: "Legend not found"});
+    }
+});
 
 app.get('/api/levels/:levelName', (req, res) => {
     const levelName = req.params.levelName;
