1823d574e1ac4db46024272118299961f323220b
Jozka Tjeris <jozkant@gmail.com>
2025-06-28 23:26:11 +0700

    feat: added support for saving json data of level into a json file

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const stringify = require('json-stringify-pretty-compact').default;
 
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
@@ -19,6 +20,24 @@ app.get('/api/legends.json', (req, res) => {
     }
     else{
         res.status(404).json({error: "Legend not found"});
+    }
+});
+
+app.post('/api/save-level', (req, res) => {
+    const levelData = req.body;
+    if(levelData){
+        const jsonString = stringify(levelData, { maxLength: 60 });
+        try{
+            fs.writeFileSync('./exported_level.json', jsonString);
+            res.status(200).json({ message: 'Level saved successfully' });
+        }
+        catch(err){
+            console.error('Error writing file:', err);
+            res.status(500).json({ error: 'Failed to write level data to file' });
+        }
+    }
+    else{
+        res.status(400).json({error: "Level Data can't be saved; invalid or missing"});
     }
 });
 
