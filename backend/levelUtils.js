adde7e723098bcec2ba0bd66501fad74e8555acb
Jozka Tjeris <jozkant@gmail.com>
2025-09-18 13:43:21 +1000

    bug: fixed backend paths

--- backend/levelUtils.js
+++ backend/levelUtils.js
@@ -2,31 +2,32 @@ import fs from "fs";
 import path from "path";
 import stringify from "json-stringify-pretty-compact";
 
-const legendsPath = path.resolve("./legends.json");
-const levelsPath = path.resolve("./levels");
+const legendsPath = path.resolve("./backend/legends.json");
+const levelsPath = path.resolve("./backend/levels");
 
-export function getLegends() {
-  if (fs.existsSync(legendsPath)) {
-    const data = fs.readFileSync(legendsPath, "utf-8");
-    return JSON.parse(data);
-  }
-  throw new Error("Legend not found");
+export function getLegends(){
+    console.log("HI", legendsPath)
+    if(fs.existsSync(legendsPath)){
+        const data = fs.readFileSync(legendsPath, "utf-8");
+        return JSON.parse(data);
+    }
+    throw new Error("Legend not found");
 }
 
-export function getLevel(levelName) {
-  const levelPath = path.resolve(`${levelsPath}/${levelName}.json`);
-  if (fs.existsSync(levelPath)) {
-    const data = fs.readFileSync(levelPath, "utf-8");
-    return JSON.parse(data);
-  }
-  throw new Error("Level not found");
+export function getLevel(levelName){
+    const levelPath = path.resolve(`${levelsPath}/${levelName}.json`);
+    if(fs.existsSync(levelPath)){
+        const data = fs.readFileSync(levelPath, "utf-8");
+        return JSON.parse(data);
+    }
+    throw new Error("Level not found");
 }
 
-export function saveLevel(levelData) {
-  if (!levelData) throw new Error("Invalid level data");
-  const jsonString = stringify(levelData, { maxLength: 60 });
+export function saveLevel(levelData){
+    if(!levelData) throw new Error("Invalid level data");
+    const jsonString = stringify(levelData, { maxLength: 60 });
 
-  fs.writeFileSync("./exported_level.json", jsonString);
+    fs.writeFileSync("./exported_level.json", jsonString);
 
-  return { message: "Level saved successfully" };
+    return { message: "Level saved successfully" };
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getLegends, getLevel, saveLevel } from "./utils/levelUtils.js";
+import { getLegends, getLevel, saveLevel } from "./levelUtils.js";
 
 const app = express();
 const PORT = 3000;
@@ -9,30 +9,33 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/api/legends.json", (req, res) => {
-  try {
-    res.json(getLegends());
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+    try{
+        res.json(getLegends());
+    }
+    catch (err){
+        res.status(404).json({ error: err.message });
+    }
 });
 
 app.get("/api/levels/:levelName", (req, res) => {
-  try {
-    res.json(getLevel(req.params.levelName));
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+    try{
+        res.json(getLevel(req.params.levelName));
+    }
+    catch (err){
+        res.status(404).json({ error: err.message });
+    }
 });
 
 app.post("/api/save-level", (req, res) => {
-  try {
-    const result = saveLevel(req.body);
-    res.json(result);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try{
+        const result = saveLevel(req.body);
+        res.json(result);
+    }
+    catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Express server running at http://localhost:${PORT}`);
+    console.log(`Express server running at http://localhost:${PORT}`);
 });
